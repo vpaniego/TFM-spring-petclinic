45c18d20ded0ed5086d6bfaf6c52dd57eaaf59bc
Roberto <roberto.martin.escribano@alumnos.upm.es>
2020-06-02 17:11:08 +0200

    Changed yaml to use kubernete health check and use kubeDNS

--- kubernetes/init-kubernetes-spring-petclinic.sh
+++ kubernetes/init-kubernetes-spring-petclinic.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 kubectl apply -f ./spring-petclinic-config-server/config-server-deployment.yaml -f ./spring-petclinic-config-server/config-server-service.yaml
-sleep 8
+sleep 10
 
-kubectl apply -f ./spring-petclinic-discovery-server/discovery-server-deployment.yaml -f ./spring-petclinic-discovery-server/discovery-server-service.yaml
 kubectl apply -f ./tracing-server/tracing-server-deployment.yaml -f ./tracing-server/tracing-server-service.yaml
-sleep 5
+sleep 10
 
 kubectl apply -f ./spring-petclinic-customers-service/customers-service-deployment.yaml -f ./spring-petclinic-customers-service/customers-service-service.yaml
 kubectl apply -f ./spring-petclinic-visits-service/visits-service-deployment.yaml -f ./spring-petclinic-visits-service/visits-service-service.yaml
@@ -13,6 +12,6 @@ kubectl apply -f ./spring-petclinic-vets-service/vets-service-deployment.yaml -f
 kubectl apply -f ./spring-petclinic-api-gateway/api-gateway-deployment.yaml -f ./spring-petclinic-api-gateway/api-gateway-service.yaml
 sleep 2
 
-kubectl apply -f ./spring-petclinic-admin-server/admin-server-deployment.yaml -f ./spring-petclinic-admin-server/admin-server-service.yaml
+#kubectl apply -f ./spring-petclinic-admin-server/admin-server-deployment.yaml -f ./spring-petclinic-admin-server/admin-server-service.yaml
 
 echo Started kubernetes-petclinic
