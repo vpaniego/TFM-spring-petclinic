ce16177e3222c1d0b15c47bdf708475fea55c13d
Roberto <roberto.martin.escribano@alumnos.upm.es>
2020-06-04 19:55:24 +0200

    issue #1 Organized imports

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -15,10 +15,12 @@
  */
 package org.springframework.samples.petclinic.admin;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
