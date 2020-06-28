746300554302695bbd364379eeb10b3319a35e3e
Roberto <roberto.martin.escribano@alumnos.upm.es>
2020-06-28 22:45:45 +0200

    issue #7 Changed kubernetes .yaml files for spring-petclinic to use new version 2.2.3-SNAPSHOT

--- kubernetes/map-kubernetes-spring-petclinic-to-local-ports.sh
+++ kubernetes/map-kubernetes-spring-petclinic-to-local-ports.sh
@@ -3,8 +3,8 @@
 #kubectl port-forward service/tfm-config-server-service      8888:8888 &
 
 kubectl port-forward service/tfm-api-gateway-service        8080:8080 &
-kubectl port-forward service/tfm-customers-service-service  8081:8081 &
-kubectl port-forward service/tfm-visits-service-service     8082:8082 &
-kubectl port-forward service/tfm-vets-service-service       8083:8083 &
+#kubectl port-forward service/tfm-customers-service-service  8081:8081 &
+#kubectl port-forward service/tfm-visits-service-service     8082:8082 &
+#kubectl port-forward service/tfm-vets-service-service       8083:8083 &
 kubectl port-forward service/tfm-admin-server-service       9090:9090 &
 kubectl port-forward service/tfm-tracing-server-service     9411:9411 &
